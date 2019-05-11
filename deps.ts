b2cea4b039d65da6c439bb3f4f4dc02c7f355998
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-05-11 17:19:59 +0200

    bump deno_std to v0.4.0

--- deps.ts
+++ deps.ts
@@ -1,14 +1,9 @@
-export { copyBytes } from "https://deno.land/x/std@v0.3.2/io/util.ts";
+export { copyBytes } from "https://deno.land/x/std@v0.4.0/io/util.ts";
 
 export {
   test,
   runTests,
   TestFunction
-} from "https://deno.land/x/std@v0.3.2/testing/mod.ts";
+} from "https://deno.land/x/std@v0.4.0/testing/mod.ts";
 
-export { assertEquals } from "https://deno.land/std@v0.3.2/testing/asserts.ts";
-
-export {
-  defer,
-  Deferred
-} from "https://deno.land/x/std@v0.2.11/util/deferred.ts";
+export { assertEquals } from "https://deno.land/std@v0.4.0/testing/asserts.ts";
