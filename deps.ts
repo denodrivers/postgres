4d4ce646aa1c0ea132670958b276aa6dbdcd8755
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-07-08 15:30:27 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,18 +1,18 @@
-export { BufReader, BufWriter } from "https://deno.land/std@v0.9.0/io/bufio.ts";
+export { BufReader, BufWriter } from "https://deno.land/std@v0.11.0/io/bufio.ts";
 
-export { copyBytes } from "https://deno.land/std@v0.9.0/io/util.ts";
+export { copyBytes } from "https://deno.land/std@v0.11.0/io/util.ts";
 
 export {
   assert,
   assertEquals,
   assertStrContains,
   assertThrowsAsync
-} from "https://deno.land/std@v0.9.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.11.0/testing/asserts.ts";
 
 export {
   runTests,
   test,
   TestFunction
-} from "https://deno.land/std@v0.9.0/testing/mod.ts";
+} from "https://deno.land/std@v0.11.0/testing/mod.ts";
 
 export { Hash } from "https://deno.land/x/checksum@1.0.0/mod.ts";
