e8bd5e08b306d18e54169ba84a7e24fbcb15fb9d
Dennis Collective <dennis.collective@gmail.com>
2019-09-17 03:55:25 -0700

    I didn't know about Record<T> :)

--- utils.ts
+++ utils.ts
@@ -73,7 +73,7 @@ export interface DsnResult {
 export function parseDsn(dsn: string): DsnResult {
   const url = new URL(dsn);
 
-  const params: { [key: string]: string } = {};
+  const params: Record<string, string> = {};
   for (const [key, value] of url.searchParams.entries()) {
     params[key] = value;
   }
