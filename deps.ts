550bf5f6c2aabb813f43a2b108c77db5f04613dd
Bartek Iwańczuk <biwanczuk@gmail.com>
2019-12-13 19:36:53 +0100

    chore: bump Deno and deps to v0.26.0 (#78)

--- deps.ts
+++ deps.ts
@@ -1,26 +1,26 @@
 export {
   BufReader,
   BufWriter
-} from "https://deno.land/std@v0.22.0/io/bufio.ts";
+} from "https://deno.land/std@v0.26.0/io/bufio.ts";
 
-export { copyBytes } from "https://deno.land/std@v0.22.0/io/util.ts";
+export { copyBytes } from "https://deno.land/std@v0.26.0/io/util.ts";
 
 export {
   assert,
   assertEquals,
   assertStrContains,
   assertThrowsAsync
-} from "https://deno.land/std@v0.22.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.26.0/testing/asserts.ts";
 
 export {
   runTests,
   test,
   TestFunction
-} from "https://deno.land/std@v0.22.0/testing/mod.ts";
+} from "https://deno.land/std@v0.26.0/testing/mod.ts";
 
 export {
   Deferred,
   deferred
-} from "https://deno.land/std@v0.22.0/util/async.ts";
+} from "https://deno.land/std@v0.26.0/util/async.ts";
 
 export { Hash } from "https://deno.land/x/checksum@1.0.1/mod.ts";
