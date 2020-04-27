3dd86a1a8b8a5aae727b99fe423d6227049cff6e
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-04-27 12:51:08 +0200

    chore: bump ci and deps to 0.41.0 (#111)

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 export {
   BufReader,
   BufWriter,
-} from "https://deno.land/std@v0.39.0/io/bufio.ts";
+} from "https://deno.land/std@v0.41.0/io/bufio.ts";
 
-export { copyBytes } from "https://deno.land/std@v0.39.0/io/util.ts";
+export { copyBytes } from "https://deno.land/std@v0.41.0/io/util.ts";
 
 export {
   Deferred,
   deferred,
-} from "https://deno.land/std@v0.39.0/util/async.ts";
+} from "https://deno.land/std@v0.41.0/util/async.ts";
 
 export { Hash } from "https://deno.land/x/checksum@1.2.0/mod.ts";
