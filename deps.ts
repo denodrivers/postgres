d8a5f4a53c4f4d09d15831917a01e59a73194dc9
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-05-14 15:35:59 +0200

    fix import

--- deps.ts
+++ deps.ts
@@ -8,6 +8,6 @@ export { copyBytes } from "https://deno.land/std@0.51.0/io/util.ts";
 export {
   Deferred,
   deferred,
-} from "https://deno.land/std@0.51.0/util/async.ts";
+} from "https://deno.land/std@0.51.0/async/deferred.ts";
 
 export { Hash } from "https://deno.land/x/checksum@1.2.0/mod.ts";
