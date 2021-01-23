7c489ea7b4451660c6a16af38876c67db4da4404
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-01-23 14:23:47 -0500

    Fix std version

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export { BufReader, BufWriter } from "https://deno.land/std@0.80.0/io/bufio.ts";
 export { copy } from "https://deno.land/std@0.80.0/bytes/mod.ts";
 export { createHash } from "https://deno.land/std@0.80.0/hash/mod.ts";
 export { deferred, delay } from "https://deno.land/std@0.80.0/async/mod.ts";
-export { bold, yellow } from "https://deno.land/std@0.84.0/fmt/colors.ts";
+export { bold, yellow } from "https://deno.land/std@0.80.0/fmt/colors.ts";
 export type { Deferred } from "https://deno.land/std@0.80.0/async/mod.ts";
