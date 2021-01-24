f79b8ef3a48fdc7707c8f611ea838207293c162e
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-01-24 18:46:30 -0500

    1.7.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { BufReader, BufWriter } from "https://deno.land/std@0.80.0/io/bufio.ts";
-export { copy } from "https://deno.land/std@0.80.0/bytes/mod.ts";
-export { createHash } from "https://deno.land/std@0.80.0/hash/mod.ts";
-export { deferred, delay } from "https://deno.land/std@0.80.0/async/mod.ts";
-export { bold, yellow } from "https://deno.land/std@0.80.0/fmt/colors.ts";
-export type { Deferred } from "https://deno.land/std@0.80.0/async/mod.ts";
+export { BufReader, BufWriter } from "https://deno.land/std@0.84.0/io/bufio.ts";
+export { copy } from "https://deno.land/std@0.84.0/bytes/mod.ts";
+export { createHash } from "https://deno.land/std@0.84.0/hash/mod.ts";
+export { deferred, delay } from "https://deno.land/std@0.84.0/async/mod.ts";
+export { bold, yellow } from "https://deno.land/std@0.84.0/fmt/colors.ts";
+export type { Deferred } from "https://deno.land/std@0.84.0/async/mod.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -4,8 +4,8 @@ export {
   assertEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.80.0/testing/asserts.ts";
+} from "https://deno.land/std@0.84.0/testing/asserts.ts";
 export {
   decode as decodeBase64,
   encode as encodeBase64,
-} from "https://deno.land/std@0.80.0/encoding/base64.ts";
+} from "https://deno.land/std@0.84.0/encoding/base64.ts";
