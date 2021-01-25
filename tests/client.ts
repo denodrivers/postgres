5472688a1caf9aa8e8861c4303a8d6148e2b6645
Steven Guerrero <stephenguerrero43@gmail.com>
2021-01-24 23:26:47 -0500

    fix: Handle startup errors (#208)
    This handles startup errors instead of throwing a generic error
    This solves an issue where specifying a wrong database on connection parameters won't throw the appropiate error

--- tests/client.ts
+++ tests/client.ts
@@ -1,23 +1,41 @@
-const { test } = Deno;
 import { Client, PostgresError } from "../mod.ts";
-import { assert } from "../test_deps.ts";
+import { assertThrowsAsync } from "../test_deps.ts";
 import TEST_CONNECTION_PARAMS from "./config.ts";
 
-test("badAuthData", async function () {
+function getRandomString() {
+  return Math.random().toString(36).substring(7);
+}
+
+Deno.test("badAuthData", async function () {
   const badConnectionData = { ...TEST_CONNECTION_PARAMS };
-  badConnectionData.password += "foobar";
+  badConnectionData.password += getRandomString();
   const client = new Client(badConnectionData);
 
-  let thrown = false;
+  await assertThrowsAsync(
+    async (): Promise<void> => {
+      await client.connect();
+    },
+    PostgresError,
+    "password authentication failed for user",
+  )
+    .finally(async () => {
+      await client.end();
+    });
+});
 
-  try {
-    await client.connect();
-  } catch (e) {
-    thrown = true;
-    assert(e instanceof PostgresError);
-    assert(e.message.includes("password authentication failed for user"));
-  } finally {
-    await client.end();
-  }
-  assert(thrown);
+Deno.test("startupError", async function () {
+  const badConnectionData = { ...TEST_CONNECTION_PARAMS };
+  badConnectionData.database += getRandomString();
+  const client = new Client(badConnectionData);
+
+  await assertThrowsAsync(
+    async (): Promise<void> => {
+      await client.connect();
+    },
+    PostgresError,
+    "does not exist",
+  )
+    .finally(async () => {
+      await client.end();
+    });
 });
