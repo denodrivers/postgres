9f5a375f0a43c58f8ea5486a28d660d23b925163
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-01-30 15:47:57 -0500

    Document query methods

--- client.ts
+++ client.ts
@@ -9,6 +9,28 @@ import {
   QueryResult,
 } from "./query.ts";
 
+// TODO
+// Limit the type of parameters that can be passed
+// to a query
+/**
+ * https://www.postgresql.org/docs/current/sql-prepare.html
+ * 
+ * This arguments will be appended to the prepared statement passed
+ * as query
+ * 
+ * They will take the position according to the order in which they were provided
+ * 
+ * ```ts
+ * await my_client.queryArray(
+ *  "SELECT ID, NAME FROM PEOPLE WHERE AGE > $1 AND AGE < $2",
+ *  10, // $1
+ *  20, // $2
+ * );
+ * ```
+ * */
+// deno-lint-ignore no-explicit-any
+type QueryArguments = any[];
+
 export class QueryClient {
   /**
    * This function is meant to be replaced when being extended
@@ -22,10 +44,23 @@ export class QueryClient {
     );
   }
 
+  /**
+   * This method allows executed queries to be retrieved as array entries.
+   * It supports a generic interface in order to type the entries retrieved by the query
+   * 
+   * ```ts
+   * const {rows} = await my_client.queryArray(
+   *  "SELECT ID, NAME FROM CLIENTS"
+   * ); // Array<unknown[]>
+   * 
+   * const {rows} = await my_client.queryArray<[number, string]>(
+   *  "SELECT ID, NAME FROM CLIENTS"
+   * ); // Array<[number, string]>
+   * ```
+   */
   queryArray<T extends Array<unknown> = Array<unknown>>(
     text: string | QueryConfig,
-    // deno-lint-ignore no-explicit-any
-    ...args: any[]
+    ...args: QueryArguments
   ): Promise<QueryArrayResult<T>> {
     let query;
     if (typeof text === "string") {
@@ -40,12 +75,43 @@ export class QueryClient {
     ) as Promise<QueryArrayResult<T>>;
   }
 
+  /**
+   * This method allows executed queries to be retrieved as object entries.
+   * It supports a generic interface in order to type the entries retrieved by the query
+   * 
+   * ```ts
+   * const {rows} = await my_client.queryObject(
+   *  "SELECT ID, NAME FROM CLIENTS"
+   * ); // Record<string, unknown>
+   * 
+   * const {rows} = await my_client.queryObject<{id: number, name: string}>(
+   *  "SELECT ID, NAME FROM CLIENTS"
+   * ); // Array<{id: number, name: string}>
+   * ```
+   * 
+   * You can also map the expected results to object fields using the configuration interface.
+   * This will be assigned in the order they were provided
+   * 
+   * ```ts
+   * const {rows} = await my_client.queryObject(
+   *  "SELECT ID, NAME FROM CLIENTS"
+   * );
+   * 
+   * console.log(rows); // [{id: 78, name: "Frank"}, {id: 15, name: "Sarah"}]
+   * 
+   * const {rows} = await my_client.queryObject({
+   *  text: "SELECT ID, NAME FROM CLIENTS",
+   *  fields: ["personal_id", "complete_name"],
+   * });
+   * 
+   * console.log(rows); // [{personal_id: 78, complete_name: "Frank"}, {personal_id: 15, complete_name: "Sarah"}]
+   * ```
+   */
   queryObject<
     T extends Record<string, unknown> = Record<string, unknown>,
   >(
     text: string | QueryObjectConfig,
-    // deno-lint-ignore no-explicit-any
-    ...args: any[]
+    ...args: QueryArguments
   ): Promise<QueryObjectResult<T>> {
     let query;
     if (typeof text === "string") {
