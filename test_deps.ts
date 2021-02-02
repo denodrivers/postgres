8859ca4429af6abe9c502a1edc6ef4f3cd8e80f7
iugo <iugogogo@gmail.com>
2021-02-03 00:45:33 +0800

    feat: Update to Deno 1.7.1 and std 0.85.0 (#240)

--- test_deps.ts
+++ test_deps.ts
@@ -4,12 +4,12 @@ export {
   assertEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.84.0/testing/asserts.ts";
+} from "https://deno.land/std@0.85.0/testing/asserts.ts";
 export {
   decode as decodeBase64,
   encode as encodeBase64,
-} from "https://deno.land/std@0.84.0/encoding/base64.ts";
+} from "https://deno.land/std@0.85.0/encoding/base64.ts";
 export {
   format as formatDate,
   parse as parseDate,
-} from "https://deno.land/std@0.84.0/datetime/mod.ts";
+} from "https://deno.land/std@0.85.0/datetime/mod.ts";
