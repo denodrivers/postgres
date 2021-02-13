49cc7435a1a3098e433ab1baf2d1df2e7663e0b0
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-02-12 19:44:20 -0500

    Remove the need for test.ts

--- tests/test.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S deno test --fail-fast --allow-net --allow-env --allow-read=tests/config.json --unstable tests/test.ts
-import "../tests/data_types.ts";
-import "../tests/queries.ts";
-import "../tests/connection_params.ts";
-import "../tests/client.ts";
-import "../tests/pool.ts";
-import "../tests/utils.ts";
