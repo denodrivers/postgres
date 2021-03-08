580b00d6aa7b3e3147e23ec8e9feaab6d61f282d
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-03-07 22:40:32 -0500

    Test no tls authentication methods

--- tests/connection_test.ts
+++ tests/connection_test.ts
@@ -1,12 +1,25 @@
-import { Client, PostgresError } from "../mod.ts";
-import { assertThrowsAsync } from "./test_deps.ts";
-import { getMainConfiguration } from "./config.ts";
+import {
+  assertEquals,
+  assertThrowsAsync,
+} from "./test_deps.ts";
+import {
+  getClearConfiguration,
+  getMainConfiguration,
+  getMd5Configuration,
+} from "./config.ts";
+import {Client, PostgresError} from "../mod.ts";
 
 function getRandomString() {
   return Math.random().toString(36).substring(7);
 }
 
-Deno.test("badAuthData", async function () {
+Deno.test("Clear password authentication (no tls)", async () => {
+  const client = new Client(getClearConfiguration());
+  await client.connect();
+  await client.end();
+});
+
+Deno.test("Handles bad authentication correctly", async function () {
   const badConnectionData = getMainConfiguration();
   badConnectionData.password += getRandomString();
   const client = new Client(badConnectionData);
@@ -23,9 +36,15 @@ Deno.test("badAuthData", async function () {
     });
 });
 
+Deno.test("MD5 authentication (no tls)", async () => {
+  const client = new Client(getMd5Configuration());
+  await client.connect();
+  await client.end();
+});
+
 // This test requires current user database connection permissions
 // on "pg_hba.conf" set to "all"
-Deno.test("startupError", async function () {
+Deno.test("Startup error when database does not exist", async function () {
   const badConnectionData = getMainConfiguration();
   badConnectionData.database += getRandomString();
   const client = new Client(badConnectionData);
