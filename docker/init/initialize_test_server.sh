1137adc691542ff9d4876f540331ff5610cd6646
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-03-08 20:16:23 -0500

    Config

--- docker/init/initialize_test_server.sh
+++ docker/init/initialize_test_server.sh
@@ -1,1 +1,7 @@
+# Prepend old postgresql.conf to new before copying everything
+cat /var/lib/postgresql/host/postgresql.conf >> /var/lib/postgresql/data/postgresql.conf
+cp /var/lib/postgresql/data/postgresql.conf /var/lib/postgresql/host/postgresql.conf
+
 cp /var/lib/postgresql/host/* /var/lib/postgresql/data
+chmod 600 /var/lib/postgresql/data/server.crt
+chmod 600 /var/lib/postgresql/data/server.key
