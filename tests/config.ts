e9b625310fd45e45a24fbdd4a60de2849b1ed395
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-04-03 16:49:38 -0500

    Create new container for SCRAM authentication

--- tests/config.ts
+++ tests/config.ts
@@ -17,59 +17,70 @@ try {
 }
 
 const config: {
-  applicationName: string;
-  database: string;
-  hostname: string;
-  password: string;
-  port: string | number;
-  users: {
-    clear: string;
-    main: string;
-    md5: string;
-    scram: string;
+  postgres: {
+    applicationName: string;
+    database: string;
+    hostname: string;
+    password: string;
+    port: string | number;
+    users: {
+      clear: string;
+      main: string;
+      md5: string;
+    };
+  };
+  postgres_scram: {
+    applicationName: string;
+    database: string;
+    hostname: string;
+    password: string;
+    port: string | number;
+    users: {
+      scram: string;
+    };
   };
 } = JSON.parse(content);
 
 export const getClearConfiguration = (): ConnectionOptions => {
   return {
-    applicationName: config.applicationName,
-    database: config.database,
-    hostname: config.hostname,
-    password: config.password,
-    port: config.port,
-    user: config.users.main,
+    applicationName: config.postgres.applicationName,
+    database: config.postgres.database,
+    hostname: config.postgres.hostname,
+    password: config.postgres.password,
+    port: config.postgres.port,
+    user: config.postgres.users.clear,
   };
 };
 
 export const getMainConfiguration = (): ConnectionOptions => {
   return {
-    applicationName: config.applicationName,
-    database: config.database,
-    hostname: config.hostname,
-    password: config.password,
-    port: config.port,
-    user: config.users.main,
+    applicationName: config.postgres.applicationName,
+    database: config.postgres.database,
+    hostname: config.postgres.hostname,
+    password: config.postgres.password,
+    port: config.postgres.port,
+    user: config.postgres.users.main,
   };
 };
 
 export const getMd5Configuration = (): ConnectionOptions => {
   return {
-    applicationName: config.applicationName,
-    database: config.database,
-    hostname: config.hostname,
-    password: config.password,
-    port: config.port,
-    user: config.users.main,
+    applicationName: config.postgres.applicationName,
+    database: config.postgres.database,
+    hostname: config.postgres.hostname,
+    password: config.postgres.password,
+    port: config.postgres.port,
+    user: config.postgres.users.md5,
   };
 };
 
 export const getScramSha256Configuration = (): ConnectionOptions => {
   return {
-    applicationName: config.applicationName,
-    database: config.database,
-    hostname: config.hostname,
-    password: config.password,
-    port: config.port,
-    user: config.users.scram,
+    applicationName: config.postgres_scram.applicationName,
+    database: config.postgres_scram.database,
+    hostname: config.postgres_scram.hostname,
+    password: config.postgres_scram.password,
+    port: config.postgres_scram.port,
+    user: config.postgres_scram.users.scram,
   };
 };
