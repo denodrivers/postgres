fa1c966461f1ca4e510fd765849653c0a0628613
Nicolas Guerrero <stephenguerrero43@gmail.com>
2021-04-11 22:06:43 -0500

    Check exports on doc.deno.land

--- mod.ts
+++ mod.ts
@@ -1,3 +1,11 @@
 export { Client } from "./client.ts";
 export { PostgresError } from "./connection/warning.ts";
 export { Pool } from "./pool.ts";
+
+// TODO
+// Remove the following reexports after https://doc.deno.land
+// supports two level depth exports
+export { PoolClient, QueryClient } from "./client.ts";
+export type { QueryConfig, QueryObjectConfig } from "./query/query.ts";
+export { Transaction } from "./query/transaction.ts";
+export type { TransactionOptions } from "./query/transaction.ts";
