5b3f603d5372156bbc949cb6e1a3f97fab655d05
Luca Casonato <hello@lcas.dev>
2021-07-21 23:59:55 +0200

    remove StartTlsOptions

--- connection/connection.ts
+++ connection/connection.ts
@@ -251,7 +251,7 @@ export class Connection {
 
   async #createTlsConnection(
     connection: Deno.Conn,
-    options: Deno.StartTlsOptions,
+    options: { hostname: string; certFile?: string },
   ) {
     if ("startTls" in Deno) {
       // @ts-ignore This API should be available on unstable
@@ -311,7 +311,10 @@ export class Connection {
      * */
     if (accepts_tls) {
       try {
-        await this.#createTlsConnection(this.#conn, { hostname, certFile: caFile });
+        await this.#createTlsConnection(this.#conn, {
+          hostname,
+          certFile: caFile,
+        });
         this.#tls = true;
       } catch (e) {
         if (!enforceTLS) {
