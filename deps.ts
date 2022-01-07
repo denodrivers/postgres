53c5ea13d1569902c2ae718bc98f8a5ad94b794d
Steven Guerrero <stephenguerrero43@gmail.com>
2022-01-06 22:12:04 -0500

    chore: Use subtle crypto instead of std hash for SCRAM (#362)

--- deps.ts
+++ deps.ts
@@ -6,7 +6,6 @@ export {
 } from "https://deno.land/std@0.114.0/io/buffer.ts";
 export { copy } from "https://deno.land/std@0.114.0/bytes/mod.ts";
 export { createHash } from "https://deno.land/std@0.114.0/hash/mod.ts";
-export { HmacSha256 } from "https://deno.land/std@0.114.0/hash/sha256.ts";
 export { deferred, delay } from "https://deno.land/std@0.114.0/async/mod.ts";
 export type { Deferred } from "https://deno.land/std@0.114.0/async/mod.ts";
 export { bold, yellow } from "https://deno.land/std@0.114.0/fmt/colors.ts";
