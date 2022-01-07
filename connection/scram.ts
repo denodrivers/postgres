10de6bb64547fd9c7dfbc7da781e21ca7cde9f48
Nicolas Guerrero <stephenguerrero43@gmail.com>
2022-01-06 20:23:06 -0500

    Simplify 1

--- connection/scram.ts
+++ connection/scram.ts
@@ -1,12 +1,31 @@
-import { base64, HmacSha256 } from "../deps.ts";
+import { base64 } from "../deps.ts";
 
-function assert(cond: unknown): asserts cond {
-  if (!cond) {
-    throw new Error("assertion failed");
-  }
+/** Number of random bytes used to generate a nonce */
+const defaultNonceSize = 16;
+const text_encoder = new TextEncoder();
+
+enum AuthenticationState {
+  Init,
+  ClientChallenge,
+  ServerChallenge,
+  ClientResponse,
+  ServerResponse,
+  Failed,
 }
 
-/** Reason of authentication failure. */
+/**
+ * Collection of SCRAM authentication keys derived from a plaintext password
+ * in HMAC-derived binary format
+ */
+interface KeySignatures {
+  client: Uint8Array;
+  server: Uint8Array;
+  stored: Uint8Array;
+}
+
+/**
+ * Reason of authentication failure
+ */
 export enum Reason {
   BadMessage = "server sent an ill-formed message",
   BadServerNonce = "server sent an invalid nonce",
@@ -16,18 +35,126 @@ export enum Reason {
   Rejected = "rejected by server",
 }
 
-/** SCRAM authentication state. */
-enum State {
-  Init,
-  ClientChallenge,
-  ServerChallenge,
-  ClientResponse,
-  ServerResponse,
-  Failed,
+function assert(cond: unknown): asserts cond {
+  if (!cond) {
+    throw new Error("Scram protocol assertion failed");
+  }
 }
 
-/** Number of random bytes used to generate a nonce. */
-const defaultNonceSize = 16;
+// TODO
+// Handle mapping and maybe unicode normalization.
+// Add tests for invalid string values
+/**
+ * Normalizes string per SASLprep.
+ * @see {@link https://tools.ietf.org/html/rfc3454}
+ * @see {@link https://tools.ietf.org/html/rfc4013}
+ */
+function assertValidScramString(str: string) {
+  const unsafe = /[^\x21-\x7e]/;
+  if (unsafe.test(str)) {
+    throw new Error(
+      "scram username/password is currently limited to safe ascii characters",
+    );
+  }
+}
+
+async function computeScramSignature(
+  message: string,
+  raw_key: Uint8Array,
+): Promise<Uint8Array> {
+  const key = await crypto.subtle.importKey(
+    "raw",
+    raw_key,
+    { name: "HMAC", hash: "SHA-256" },
+    false,
+    ["sign"],
+  );
+
+  return new Uint8Array(
+    await crypto.subtle.sign(
+      { name: "HMAC", hash: "SHA-256" },
+      key,
+      text_encoder.encode(message),
+    ),
+  );
+}
+
+function computeScramProof(signature: Uint8Array, key: Uint8Array): Uint8Array {
+  const digest = new Uint8Array(signature.length);
+  for (let i = 0; i < digest.length; i++) {
+    digest[i] = signature[i] ^ key[i];
+  }
+  return digest;
+}
+
+/**
+ * Derives authentication key signatures from a plaintext password
+ */
+async function deriveKeySignatures(
+  password: string,
+  salt: Uint8Array,
+  iterations: number,
+): Promise<KeySignatures> {
+  const pbkdf2_password = await crypto.subtle.importKey(
+    "raw",
+    text_encoder.encode(password),
+    "PBKDF2",
+    false,
+    ["deriveBits", "deriveKey"],
+  );
+  const key = await crypto.subtle.deriveKey(
+    {
+      hash: "SHA-256",
+      iterations,
+      name: "PBKDF2",
+      salt,
+    },
+    pbkdf2_password,
+    { name: "HMAC", hash: "SHA-256" },
+    false,
+    ["sign"],
+  );
+
+  const client = new Uint8Array(
+    await crypto.subtle.sign("HMAC", key, text_encoder.encode("Client Key")),
+  );
+  const server = new Uint8Array(
+    await crypto.subtle.sign("HMAC", key, text_encoder.encode("Server Key")),
+  );
+  const stored = new Uint8Array(await crypto.subtle.digest("SHA-256", client));
+
+  return { client, server, stored };
+}
+
+/** Escapes "=" and "," in a string. */
+function escape(str: string): string {
+  return str
+    .replace(/=/g, "=3D")
+    .replace(/,/g, "=2C");
+}
+
+function generateRandomNonce(size: number): string {
+  return base64.encode(crypto.getRandomValues(new Uint8Array(size)));
+}
+
+function parseScramAttributes(message: string): Record<string, string> {
+  const attrs: Record<string, string> = {};
+
+  for (const entry of message.split(",")) {
+    const pos = entry.indexOf("=");
+    if (pos < 1) {
+      throw new Error(Reason.BadMessage);
+    }
+
+    // TODO
+    // Replace with String.prototype.substring
+    const key = entry.substr(0, pos);
+    const value = entry.substr(pos + 1);
+    attrs[key] = value;
+  }
+
+  return attrs;
+}
 
 /**
  * Client composes and verifies SCRAM authentication messages, keeping track
@@ -35,56 +162,61 @@ const defaultNonceSize = 16;
  * @see {@link https://tools.ietf.org/html/rfc5802}
  */
 export class Client {
-  #authMessage: string;
-  #clientNonce: string;
-  #keys?: Keys;
+  #auth_message: string;
+  #client_nonce: string;
+  #key_signatures?: KeySignatures;
   #password: string;
-  #serverNonce?: string;
-  #state: State;
+  #server_nonce?: string;
+  #state: AuthenticationState;
   #username: string;
 
-  /** Constructor sets credentials and parameters used in an authentication. */
   constructor(username: string, password: string, nonce?: string) {
-    this.#username = username;
+    assertValidScramString(password);
+    assertValidScramString(username);
+
+    this.#auth_message = "";
+    this.#client_nonce = nonce ?? generateRandomNonce(defaultNonceSize);
     this.#password = password;
-    this.#clientNonce = nonce ?? generateNonce(defaultNonceSize);
-    this.#authMessage = "";
-    this.#state = State.Init;
+    this.#state = AuthenticationState.Init;
+    this.#username = escape(username);
   }
 
-  /** Composes client-first-message. */
+  /**
+   * Composes client-first-message
+   */
   composeChallenge(): string {
-    assert(this.#state === State.Init);
+    assert(this.#state === AuthenticationState.Init);
 
     try {
       // "n" for no channel binding, then an empty authzid option follows.
       const header = "n,,";
 
-      const username = escape(normalize(this.#username));
-      const challenge = `n=${username},r=${this.#clientNonce}`;
+      const challenge = `n=${this.#username},r=${this.#client_nonce}`;
       const message = header + challenge;
 
-      this.#authMessage += challenge;
-      this.#state = State.ClientChallenge;
+      this.#auth_message += challenge;
+      this.#state = AuthenticationState.ClientChallenge;
       return message;
     } catch (e) {
-      this.#state = State.Failed;
+      this.#state = AuthenticationState.Failed;
       throw e;
     }
   }
 
-  /** Processes server-first-message. */
+  /**
+   * Processes server-first-message
+   */
   async receiveChallenge(challenge: string) {
-    assert(this.#state === State.ClientChallenge);
+    assert(this.#state === AuthenticationState.ClientChallenge);
 
     try {
-      const attrs = parseAttributes(challenge);
+      const attrs = parseScramAttributes(challenge);
 
       const nonce = attrs.r;
-      if (!attrs.r || !attrs.r.startsWith(this.#clientNonce)) {
+      if (!attrs.r || !attrs.r.startsWith(this.#client_nonce)) {
         throw new Error(Reason.BadServerNonce);
       }
-      this.#serverNonce = nonce;
+      this.#server_nonce = nonce;
 
       let salt: Uint8Array | undefined;
       if (!attrs.s) {
@@ -101,191 +233,81 @@ export class Client {
         throw new Error(Reason.BadIterationCount);
       }
 
-      this.#keys = await deriveKeys(this.#password, salt, iterCount);
+      this.#key_signatures = await deriveKeySignatures(
+        this.#password,
+        salt,
+        iterCount,
+      );
 
-      this.#authMessage += "," + challenge;
-      this.#state = State.ServerChallenge;
+      this.#auth_message += "," + challenge;
+      this.#state = AuthenticationState.ServerChallenge;
     } catch (e) {
-      this.#state = State.Failed;
+      this.#state = AuthenticationState.Failed;
       throw e;
     }
   }
 
-  /** Composes client-final-message. */
+  /**
+   * Composes client-final-message
+   */
   async composeResponse(): Promise<string> {
-    assert(this.#state === State.ServerChallenge);
-    assert(this.#keys);
-    assert(this.#serverNonce);
+    assert(this.#state === AuthenticationState.ServerChallenge);
+    assert(this.#key_signatures);
+    assert(this.#server_nonce);
 
     try {
       // "biws" is the base-64 encoded form of the gs2-header "n,,".
-      const responseWithoutProof = `c=biws,r=${this.#serverNonce}`;
+      const responseWithoutProof = `c=biws,r=${this.#server_nonce}`;
 
-      this.#authMessage += "," + responseWithoutProof;
+      this.#auth_message += "," + responseWithoutProof;
 
       const proof = base64.encode(
-        computeProof(
-          await computeSignature(this.#authMessage, this.#keys.stored),
-          this.#keys.client,
+        computeScramProof(
+          await computeScramSignature(
+            this.#auth_message,
+            this.#key_signatures.stored,
+          ),
+          this.#key_signatures.client,
         ),
       );
       const message = `${responseWithoutProof},p=${proof}`;
 
-      this.#state = State.ClientResponse;
+      this.#state = AuthenticationState.ClientResponse;
       return message;
     } catch (e) {
-      this.#state = State.Failed;
+      this.#state = AuthenticationState.Failed;
       throw e;
     }
   }
 
-  /** Processes server-final-message. */
+  /**
+   * Processes server-final-message
+   */
   async receiveResponse(response: string) {
-    assert(this.#state === State.ClientResponse);
-    assert(this.#keys);
+    assert(this.#state === AuthenticationState.ClientResponse);
+    assert(this.#key_signatures);
 
     try {
-      const attrs = parseAttributes(response);
+      const attrs = parseScramAttributes(response);
 
       if (attrs.e) {
         throw new Error(attrs.e ?? Reason.Rejected);
       }
 
       const verifier = base64.encode(
-        await computeSignature(this.#authMessage, this.#keys.server),
+        await computeScramSignature(
+          this.#auth_message,
+          this.#key_signatures.server,
+        ),
       );
       if (attrs.v !== verifier) {
         throw new Error(Reason.BadVerifier);
       }
 
-      this.#state = State.ServerResponse;
+      this.#state = AuthenticationState.ServerResponse;
     } catch (e) {
-      this.#state = State.Failed;
+      this.#state = AuthenticationState.Failed;
       throw e;
     }
   }
-}
-
-/** Generates a random nonce string. */
-function generateNonce(size: number): string {
-  return base64.encode(crypto.getRandomValues(new Uint8Array(size)));
-}
-
-/** Parses attributes out of a SCRAM message. */
-function parseAttributes(str: string): Record<string, string> {
-  const attrs: Record<string, string> = {};
-
-  for (const entry of str.split(",")) {
-    const pos = entry.indexOf("=");
-    if (pos < 1) {
-      throw new Error(Reason.BadMessage);
-    }
-
-    const key = entry.substr(0, pos);
-    const value = entry.substr(pos + 1);
-    attrs[key] = value;
-  }
-
-  return attrs;
-}
-
-/** HMAC-derived binary key. */
-type Key = Uint8Array;
-
-/** Binary digest. */
-type Digest = Uint8Array;
-
-/** Collection of SCRAM authentication keys derived from a plaintext password. */
-interface Keys {
-  server: Key;
-  client: Key;
-  stored: Key;
-}
-
-/** Derives authentication keys from a plaintext password. */
-async function deriveKeys(
-  password: string,
-  salt: Uint8Array,
-  iterCount: number,
-): Promise<Keys> {
-  const pbkdf2_password = await crypto.subtle.importKey(
-    "raw",
-    bytes(normalize(password)),
-    "PBKDF2",
-    false,
-    ["deriveBits", "deriveKey"],
-  );
-  const key = await crypto.subtle.deriveKey(
-    {
-      hash: "SHA-256",
-      iterations: iterCount,
-      name: "PBKDF2",
-      salt,
-    },
-    pbkdf2_password,
-    { name: "HMAC", hash: "SHA-256" },
-    false,
-    ["sign"],
-  );
-  const client = new Uint8Array(
-    await crypto.subtle.sign("hmac", key, bytes("Client Key")),
-  );
-  const server = new Uint8Array(
-    await crypto.subtle.sign("hmac", key, bytes("Server Key")),
-  );
-  const stored = new Uint8Array(await crypto.subtle.digest("SHA-256", client));
-
-  return { client, server, stored };
-}
-
-/** Computes SCRAM signature. */
-function computeSignature(message: string, key: Key): Promise<Digest> {
-  return sign(bytes(message), key);
-}
-
-/** Computes SCRAM proof. */
-function computeProof(signature: Digest, key: Key): Digest {
-  const proof = new Uint8Array(signature.length);
-  for (let i = 0; i < proof.length; i++) {
-    proof[i] = signature[i] ^ key[i];
-  }
-  return proof;
-}
-
-/** Returns UTF-8 bytes encoding given string. */
-function bytes(str: string): Uint8Array {
-  return new TextEncoder().encode(str);
-}
-
-/**
- * Normalizes string per SASLprep.
- * @see {@link https://tools.ietf.org/html/rfc3454}
- * @see {@link https://tools.ietf.org/html/rfc4013}
- */
-function normalize(str: string): string {
-  // TODO: Handle mapping and maybe unicode normalization.
-  const unsafe = /[^\x21-\x7e]/;
-  if (unsafe.test(str)) {
-    throw new Error(
-      "scram username/password is currently limited to safe ascii characters",
-    );
-  }
-  return str;
-}
-
-/** Escapes "=" and "," in a string. */
-function escape(str: string): string {
-  return str
-    .replace(/=/g, "=3D")
-    .replace(/,/g, "=2C");
-}
-
-/** Computes HMAC of a message using given key. */
-// TODO
-// Migrate to crypto.subtle.sign on Deno 1.11
-// deno-lint-ignore require-await
-async function sign(msg: Uint8Array, key: Key): Promise<Digest> {
-  const hmac = new HmacSha256(key);
-  hmac.update(msg);
-  return new Uint8Array(hmac.arrayBuffer());
 }
