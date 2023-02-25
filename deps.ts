e0bb0798ce5ddd3e127178e3b56e42334cb1226f
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-25 21:37:11 +0800

    deps: use `bytes/copy` module directly

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@ export * as hex from "https://deno.land/std@0.180.0/encoding/hex.ts";
 export * as date from "https://deno.land/std@0.180.0/datetime/mod.ts";
 export { BufReader } from "https://deno.land/std@0.180.0/io/buf_reader.ts";
 export { BufWriter } from "https://deno.land/std@0.180.0/io/buf_writer.ts";
-export { copy } from "https://deno.land/std@0.180.0/bytes/mod.ts";
+export { copy } from "https://deno.land/std@0.180.0/bytes/copy.ts";
 export { crypto } from "https://deno.land/std@0.180.0/crypto/mod.ts";
 export {
   type Deferred,
