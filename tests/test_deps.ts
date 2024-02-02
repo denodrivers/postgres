954de25077330ad15b24a8dfce4879c21d7c2dc4
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-02-02 23:07:17 +0800

    deps!: update test dependencies

--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -6,6 +6,6 @@ export {
   assertObjectMatch,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.214.0/testing/asserts.ts";
+} from "https://deno.land/std@0.214.0/assert/mod.ts";
 export { format as formatDate } from "https://deno.land/std@0.214.0/datetime/format.ts";
-export { copy as copyStream } from "https://deno.land/std@0.214.0/streams/copy.ts";
+export { copy as copyStream } from "https://deno.land/std@0.214.0/io/copy.ts";
