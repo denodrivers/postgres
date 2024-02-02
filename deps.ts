ddfec1a8de612d59b5be8acc7208818ffa106bef
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-02-02 23:01:35 +0800

    deps!: upgrade to `std@0.214.0`

--- deps.ts
+++ deps.ts
@@ -1,18 +1,14 @@
-export * as base64 from "https://deno.land/std@0.180.0/encoding/base64.ts";
-export * as hex from "https://deno.land/std@0.180.0/encoding/hex.ts";
-export { parse as parseDate } from "https://deno.land/std@0.180.0/datetime/parse.ts";
-export { BufReader } from "https://deno.land/std@0.180.0/io/buf_reader.ts";
-export { BufWriter } from "https://deno.land/std@0.180.0/io/buf_writer.ts";
-export { copy } from "https://deno.land/std@0.180.0/bytes/copy.ts";
-export { crypto } from "https://deno.land/std@0.180.0/crypto/crypto.ts";
-export {
-  type Deferred,
-  deferred,
-} from "https://deno.land/std@0.180.0/async/deferred.ts";
-export { delay } from "https://deno.land/std@0.180.0/async/delay.ts";
-export { bold, yellow } from "https://deno.land/std@0.180.0/fmt/colors.ts";
+export * as base64 from "https://deno.land/std@0.214.0/encoding/base64.ts";
+export * as hex from "https://deno.land/std@0.214.0/encoding/hex.ts";
+export { parse as parseDate } from "https://deno.land/std@0.214.0/datetime/parse.ts";
+export { BufReader } from "https://deno.land/std@0.214.0/io/buf_reader.ts";
+export { BufWriter } from "https://deno.land/std@0.214.0/io/buf_writer.ts";
+export { copy } from "https://deno.land/std@0.214.0/bytes/copy.ts";
+export { crypto } from "https://deno.land/std@0.214.0/crypto/crypto.ts";
+export { delay } from "https://deno.land/std@0.214.0/async/delay.ts";
+export { bold, yellow } from "https://deno.land/std@0.214.0/fmt/colors.ts";
 export {
   fromFileUrl,
   isAbsolute,
   join as joinPath,
-} from "https://deno.land/std@0.180.0/path/mod.ts";
+} from "https://deno.land/std@0.214.0/path/mod.ts";
--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -6,6 +6,6 @@ export {
   assertObjectMatch,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.180.0/testing/asserts.ts";
-export { format as formatDate } from "https://deno.land/std@0.180.0/datetime/format.ts";
-export { copy as copyStream } from "https://deno.land/std@0.180.0/streams/copy.ts";
+} from "https://deno.land/std@0.214.0/testing/asserts.ts";
+export { format as formatDate } from "https://deno.land/std@0.214.0/datetime/format.ts";
+export { copy as copyStream } from "https://deno.land/std@0.214.0/streams/copy.ts";
