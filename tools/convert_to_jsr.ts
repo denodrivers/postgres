fa0f4eec86610f9af8385e8b52a2d4777d57a976
Asher Gomez <ashersaupingomez@gmail.com>
2024-02-06 16:21:02 +1100

    fmt

--- tools/convert_to_jsr.ts
+++ tools/convert_to_jsr.ts
@@ -1,11 +1,17 @@
 import { walk } from "https://deno.land/std@0.214.0/fs/walk.ts";
 import denoConfig from "../deno.json" with { type: "json" };
 
-const STD_SPECIFIER_REGEX = /https:\/\/deno\.land\/std@(\d+\.\d+\.\d+)\/(\w+)\/(.+)\.ts/g;
+const STD_SPECIFIER_REGEX =
+  /https:\/\/deno\.land\/std@(\d+\.\d+\.\d+)\/(\w+)\/(.+)\.ts/g;
 const POSTGRES_X_SPECIFIER = "https://deno.land/x/postgres/mod.ts";
 const POSTGRES_JSR_SPECIFIER = `jsr:${denoConfig.name}`;
 
-function toStdJsrSpecifier(_full: string, version: string, module: string, path: string): string {
+function toStdJsrSpecifier(
+  _full: string,
+  version: string,
+  module: string,
+  path: string,
+): string {
   return path === "mod"
     ? `jsr:@std/${module}@${version}`
     : `jsr:@std/${module}@${version}/${path}`;
